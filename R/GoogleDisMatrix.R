7bb6e1300481c56e2f958c08d11c9e1e0e669c73
Esteban Lopez <estebanlp@gmail.com>
2015-12-26 12:35:38 -0600

    Sdding example, correcting miss-spellings

--- R/GoogleDisMatrix.R
+++ R/GoogleDisMatrix.R
@@ -1,8 +1,8 @@
-GoogleDisMatrix<- function(orilat,orilot,deslat,deslot,mode){
+GoogleDisMatrix<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NULL,departure_time=NULL,arrival_time=NULL,transit_mode=NULL,transit_routing_preference=NULL){
   require(XML)
   
-  origin<-paste0(orilat,',',orilot)
-  destination<-paste0(deslat,',',deslot)
+  origin<-paste0(orilat,',',orilon)
+  destination<-paste0(deslat,',',deslon)
   mode<-paste0(mode)
   
   xml.url <- paste0('http://maps.googleapis.com/maps/api/distancematrix/xml?origins=',origin,'&destinations=',destination,'&mode=',mode,'&sensor=false')
