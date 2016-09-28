61b1d145924a96614a79ab424639c000e5d63c1f
Esteban Lopez <estebanlp@gmail.com>
2016-09-28 05:24:00 -0500

    Fixing Dependencies

--- R/GoogleDisMatrix.R
+++ R/GoogleDisMatrix.R
@@ -1,5 +1,6 @@
 GoogleDisMatrix<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NULL,departure_time=NULL,arrival_time=NULL,transit_mode=NULL,transit_routing_preference=NULL){
-  require(XML)
+  requireNamespace("XML",quietly = TRUE)
+  requireNamespace("RCurl",quietly = TRUE)  
   
   origin<-paste0(orilat,',',orilon)
   destination<-paste0(deslat,',',deslon)
@@ -7,8 +8,8 @@ GoogleDisMatrix<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NUL
   
   xml.url <- paste0('http://maps.googleapis.com/maps/api/distancematrix/xml?origins=',origin,'&destinations=',destination,'&mode=',mode,'&sensor=false')
   
-  xmlfile <- xmlTreeParse(xml.url)
-  xmltop = xmlRoot(xmlfile)
+  xmlfile <- XML::xmlTreeParse(xml.url)
+  xmltop <- XML::xmlRoot(xmlfile)
   distance <- xmltop[['row']][[1]][['distance']][['value']][[1]]
   distance <- as.numeric(unclass(distance)[['value']])
   mts <- distance 
--- R/GoogleNearbySearch.R
+++ R/GoogleNearbySearch.R
@@ -1,67 +1,66 @@
-GoogleNearbySearch<-function(query,lat,lon,radius,apikey)
+GoogleNearbySearch<-function(query,lat,lon,radius,apikey=NULL)
 {
-require(XML)
-require(RCurl)
+  requireNamespace("XML",quietly = TRUE)
+  requireNamespace("RCurl",quietly = TRUE)
+  query<-as.character(query)
+  query<-gsub(" ","+",query)
+  location<-paste0("&location=",lat,",",lon)
+  radius<-paste0("&radius=",radius)
+  core<-"https://maps.googleapis.com/maps/api/place/textsearch/xml?query="
+  corenext<-"https://maps.googleapis.com/maps/api/place/textsearch/xml?pagetoken="
+  apikey<-paste0("&key=",as.character(apikey))
 
-query<-as.character(query)
-query<-gsub(" ","+",query)
-location<-paste0("&location=",lat,",",lon)
-radius<-paste0("&radius=",radius)
-core<-"https://maps.googleapis.com/maps/api/place/textsearch/xml?query="
-corenext<-"https://maps.googleapis.com/maps/api/place/textsearch/xml?pagetoken="
-apikey<-paste0("&key=",as.character(apikey))
-
-p<-1
-while(p <= 3){
-	if(p==1){
-		xml.url<-paste0(core,query,radius,location,apikey)	
-	} else {
-		xml.url <-paste0(corenext,next_page,apikey)
-	}
-
-	Sys.sleep(5)
-	xmlfile<-xmlTreeParse(getURL(xml.url))
-	xmltop <-xmlRoot(xmlfile)
-	
-	stores<-20
-	
-	if(unclass(xmltop$children$status[[1]])[['value']]=="ZERO_RESULTS") stop("There are ZERO results for your query.")
-	if(unclass(xmltop$children$status[[1]])[['value']]=="REQUEST_DENIED") stop("Your API Key has been denied. Check your API key validity.")
-	#if(unclass(xmltop$children$status[[1]])[['value']]=="INVALID_REQUEST") stop("A required parameter is missing.")
-		
-	eval(parse(text=paste0("DATA",p,"<-as.data.frame(matrix(,stores,6))")))
-	eval(parse(text=paste0("names(DATA",p,")<-c('store_name', 'store_type', 'store_addr', 'store_lat', 'store_lon', 'store_rating')")))
-
-	for(i in 1:stores){
-		store_name<-xmltop$children[2:21][[i]][['name']][[1]]
-		store_type<-xmltop$children[2:21][[i]][['type']][[1]]
-		store_addr<-xmltop$children[2:21][[i]][['formatted_address']][[1]]
-		store_lat<-xmltop$children[2:21][[i]][['geometry']][[1]][[1]][[1]]
-		store_lon<-xmltop$children[2:21][[i]][['geometry']][[1]][[2]][[1]]
-		store_rating<-xmltop$children[2:21][[i]][['rating']][[1]]
-	
-		store_name<-as.character(unclass(store_name)[['value']])
-		store_type<-as.character(unclass(store_type)[['value']])
-		store_addr <-as.character(unclass(store_addr)[['value']])
-		store_lat <-as.numeric(unclass(store_lat)[['value']])
-		store_lon <-as.numeric(unclass(store_lon)[['value']])
-		store_rating <-as.numeric(unclass(store_rating)[['value']])
-		store_rating<-ifelse(length(store_rating)==0,0,store_rating)
-		
-		eval(parse(text=paste0("DATA",p,"[(",i,"),]<-cbind(store_name,store_type,store_addr,store_lat,store_lon,store_rating)")))
-	}
-	next_page<-unclass(xmltop$children$next_page_token[[1]])[['value']]
-	if(p>1 & is.null(next_page)==TRUE){
-		p<-4
-		DATA3<-as.data.frame(matrix(0,stores,6))
-		names(DATA3)<-names(DATA1)
-	}
-	p<-p+1
-}
-DATA<-rbind(DATA1,DATA2,DATA3)
-DATA<-DATA[which(DATA[,1]!=0),]
-return(DATA)
-rm(DATA,DATA1,DATA2,DATA3)
+  p<-1
+  while(p <= 3){
+  	if(p==1){
+  		xml.url<-paste0(core,query,radius,location,apikey)	
+  	} else {
+  		xml.url <-paste0(corenext,next_page,apikey)
+  	}
+  
+  	Sys.sleep(5)
+  	xmlfile<-XML::xmlTreeParse(RCurl::getURL(xml.url))
+  	xmltop <-XML::xmlRoot(xmlfile)
+  	
+  	stores<-20
+  	
+  	if(unclass(xmltop$children$status[[1]])[['value']]=="ZERO_RESULTS") stop("There are ZERO results for your query.")
+  	if(unclass(xmltop$children$status[[1]])[['value']]=="REQUEST_DENIED") stop("Your API Key has been denied. Check your API key validity.")
+  	#if(unclass(xmltop$children$status[[1]])[['value']]=="INVALID_REQUEST") stop("A required parameter is missing.")
+  		
+  	eval(parse(text=paste0("DATA",p,"<-as.data.frame(matrix(,stores,6))")))
+  	eval(parse(text=paste0("names(DATA",p,")<-c('store_name', 'store_type', 'store_addr', 'store_lat', 'store_lon', 'store_rating')")))
+  
+  	for(i in 1:stores){
+  		store_name<-xmltop$children[2:21][[i]][['name']][[1]]
+  		store_type<-xmltop$children[2:21][[i]][['type']][[1]]
+  		store_addr<-xmltop$children[2:21][[i]][['formatted_address']][[1]]
+  		store_lat<-xmltop$children[2:21][[i]][['geometry']][[1]][[1]][[1]]
+  		store_lon<-xmltop$children[2:21][[i]][['geometry']][[1]][[2]][[1]]
+  		store_rating<-xmltop$children[2:21][[i]][['rating']][[1]]
+  	
+  		store_name<-as.character(unclass(store_name)[['value']])
+  		store_type<-as.character(unclass(store_type)[['value']])
+  		store_addr <-as.character(unclass(store_addr)[['value']])
+  		store_lat <-as.numeric(unclass(store_lat)[['value']])
+  		store_lon <-as.numeric(unclass(store_lon)[['value']])
+  		store_rating <-as.numeric(unclass(store_rating)[['value']])
+  		store_rating<-ifelse(length(store_rating)==0,0,store_rating)
+  		
+  		eval(parse(text=paste0("DATA",p,"[(",i,"),]<-cbind(store_name,store_type,store_addr,store_lat,store_lon,store_rating)")))
+  	}
+  	next_page<-unclass(xmltop$children$next_page_token[[1]])[['value']]
+  	if(p>1 & is.null(next_page)==TRUE){
+  		p<-4
+  		DATA3<-as.data.frame(matrix(0,stores,6))
+  		names(DATA3)<-names(DATA1)
+  	}
+  	p<-p+1
+  }
+  DATA<-rbind(DATA1,DATA2,DATA3)
+  DATA<-DATA[which(DATA[,1]!=0),]
+  return(DATA)
+  rm(DATA,DATA1,DATA2,DATA3)
 }
 
 
