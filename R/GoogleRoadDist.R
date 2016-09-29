545ba10bd560a67041fefe64e92c1295fe0ffee0
Esteban Lopez <estebanlp@gmail.com>
2016-09-29 09:15:32 -0500

    updating GooglePlaceSearch and deprecating GoogleDistMatrix to GoogleRoadDist

--- R/GoogleRoadDist.R
+++ R/GoogleRoadDist.R
@@ -1,6 +1,7 @@
-GoogleDisMatrix<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NULL,departure_time=NULL,arrival_time=NULL,transit_mode=NULL,transit_routing_preference=NULL){
+GoogleRoadDist<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NULL,departure_time=NULL,arrival_time=NULL,transit_mode=NULL,transit_routing_preference=NULL){
   requireNamespace("XML",quietly = TRUE)
   requireNamespace("RCurl",quietly = TRUE)  
+  requireNamespace("stringr",quietly = TRUE)  
   
   origin<-paste0(orilat,',',orilon)
   destination<-paste0(deslat,',',deslon)
@@ -12,11 +13,11 @@ GoogleDisMatrix<- function(orilat,orilon,deslat,deslon,mode='driving',apikey=NUL
   xmltop <- XML::xmlRoot(xmlfile)
   distance <- xmltop[['row']][[1]][['distance']][['value']][[1]]
   distance <- as.numeric(unclass(distance)[['value']])
-  mts <- distance 
+  miles <- distance*0.000621371 
   duration <- xmltop[['row']][[1]][['duration']][['text']][[1]]
   duration <- unclass(duration)[['value']]
-  min<-duration
-  D<-data.frame(cbind(mts,min))
-  names(D)<-c('mts','min')
+  duration <- as.numeric(stringr::str_extract(duration,"\\d"))
+  minutes<-duration
+  D<-list(miles=miles,minutes=minutes)
   return(D)
 }
